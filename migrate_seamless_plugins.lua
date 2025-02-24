f88a5387a5702636647723256d61e517999a7844
Max Weidauer <max+git@weidauer.art>
2025-02-24 19:54:56 +0100

    added rotation and scaling

--- migrate_seamless_plugins.lua
+++ migrate_seamless_plugins.lua
@@ -13,6 +13,19 @@ param_idx_new = {4,5,6,1,0,2}
 
 new_plugin_name = "Seamless Client"
 old_plugin_name = "Seamless_Client"
+scaling_factor = 12.108
+
+-- if rotate x_rot=-y_old, y_rot=x_old
+rotate = true
+
+-- swap x and y target idx if rotate == true
+if rotate then
+  x_new_idx = param_idx_new[1]
+  y_new_idx = param_idx_new[2]
+
+  param_idx_new[1] = y_new_idx
+  param_idx_new[2] = x_new_idx
+
 
 for i = start_track,end_track do
   track = reaper.GetTrack(0, i)
@@ -24,8 +37,12 @@ for i = start_track,end_track do
   new_plugin_idx = reaper.TrackFX_AddByName(track, new_plugin_name, false, 1)
   old_plugin_idx = reaper.TrackFX_AddByName(track, old_plugin_name, false, 0)
   count = 0
+
+  -- migrate gain parameters
   for i = 0, 5 do
     old_env = reaper.GetFXEnvelope(track, old_plugin_idx, i, false)
+
+
     new_i = param_idx_new[i+1]
     new_env = reaper.GetFXEnvelope(track, new_plugin_idx,new_i , true)
     if old_env ~= nil then 
@@ -37,10 +54,26 @@ for i = start_track,end_track do
       n_env_points_new = reaper.CountEnvelopePointsEx(new_env, -1)
       
       for point_idx = 0, n_env_points_old - 1 do
+        -- get old envelope point
         retval, time, value, shape, tension, selected = reaper.GetEnvelopePointEx(old_env,-1, point_idx)
+        -- scale coordinate
+        if i <= 2 then
+          value = value / scaling_factor
+        end
+
+        -- flip y coordinate if rotating
+        if rotate and i == 1 then
+          value = value * -1
+        end
+
+        -- write point to new envelope
         reaper.InsertEnvelopePointEx(new_env, -1, time, value, shape, tension, true, true)
       end
+
+      -- sort points for some reason
       reaper.Envelope_SortPointsEx(new_env, -1)
+
+      -- did it work? lets find out
       n_env_points_new = reaper.CountEnvelopePoints(new_env)
       print("  " .. param_name_old .. " n_items:"..n_env_points_old.."\n")
       print("  " .. param_name_new .. " n_items:"..n_env_points_new.."\n")    
@@ -48,6 +81,5 @@ for i = start_track,end_track do
       print("skipped something")
     end
   end
-  
   
 end
